6eccdb2bd486515a259632422577e9a47b1d4a2e
dash-bhavinmakwana <Bhavin.M@dashtechinc.com>
2026-03-05 17:24:40 +0530

    Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,26 +1,22 @@
-variable "rg_name"  {}
-variable "location" {}
-variable "env"      {}
-
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-${var.env}"
-  address_space       = ["10.0.0.0/16"]
+  name                = "vnet-${var.project_name}-${var.env}"
+  address_space       = [var.vnet_cidr]
   location            = var.location
   resource_group_name = var.rg_name
+
+  tags = var.tags
 }
 
 resource "azurerm_subnet" "frontend" {
-  name                 = "snet-frontend"
+  name                 = "snet-frontend-${var.env}"
   resource_group_name  = var.rg_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = [var.frontend_subnet_cidr]
 }
 
 resource "azurerm_subnet" "backend" {
-  name                 = "snet-backend"
+  name                 = "snet-backend-${var.env}"
   resource_group_name  = var.rg_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = [var.backend_subnet_cidr]
 }
-
-output "vnet_id" { value = azurerm_virtual_network.vnet.id }
