e8630d1ef66cb2f0bece943c969332b9da5c31b3
dash-bhavinmakwana <Bhavin.M@dashtechinc.com>
2026-03-05 17:22:44 +0530

    Update outputs.tf

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -1,11 +1,26 @@
+output "resource_group_name" {
+  value       = azurerm_resource_group.main.name
+  description = "The name of the resource group"
+}
+
 output "flutter_app_urls" {
   value = {
-    for name, app in module.flutter_apps : name => app.default_hostname
+    for name, app in module.flutter_apps : name => "https://${app.default_hostname}"
   }
-  description = "The public URLs for the Flutter Static Web Apps"
+  description = "The public URLs of the Flutter Static Web Apps"
 }
 
 output "web_app_url" {
-  value       = module.main_web_app.default_hostname
-  description = "The public URL for the main Web App"
+  value       = "https://${module.web_app.webapp_url}"
+  description = "The public URL of the Web App"
+}
+
+output "storage_account_name" {
+  value       = module.storage.storage_account_name
+  description = "The name of the Storage Account"
+}
+
+output "vnet_id" {
+  value       = module.networking.vnet_id
+  description = "The ID of the Virtual Network"
 }
