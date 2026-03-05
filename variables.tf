e2c7bcc73647c59a375d3a2f6cbb688556810386
dash-bhavinmakwana <Bhavin.M@dashtechinc.com>
2026-03-05 17:30:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "project_name" {
-  type    = string
-  default = "salesevent"
+  type        = string
+  description = "The name of the project used in resource naming"
+  default     = "myproject"
 }
 
 variable "env" {
-  type    = string
-  default = "dev"
+  type        = string
+  description = "The deployment environment (dev, staging, prod)"
+  default     = "dev"
 }
 
 variable "location" {
-  type    = string
-  default = "East US"
+  type        = string
+  description = "Azure region for all resources"
+  default     = "East US"
 }
 
 variable "flutter_app_names" {
-  type    = list(string)
-  default = ["customer-app", "admin-app"]
+  type        = list(string)
+  description = "List of names for Flutter static web apps (exactly 2)"
+  default     = ["customer-app", "admin-app"]
 }
