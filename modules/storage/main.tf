23df25465e786225c98b6c720d590c9746def83e
dash-bhavinmakwana <Bhavin.M@dashtechinc.com>
2026-03-05 17:27:45 +0530

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,18 +1,13 @@
-variable "st_name"  {}
-variable "rg_name"  {}
-variable "location" {}
-
 resource "azurerm_storage_account" "this" {
   name                     = var.st_name
   resource_group_name      = var.rg_name
   location                 = var.location
   account_tier             = "Standard"
-  account_replication_type = "LRS" # Lowest cost
-  
-  # Allow static site hosting if needed later
-  static_website {
-    index_document = "index.html"
-  }
-}
+  account_replication_type = var.replication_type
 
-output "storage_endpoint" { value = azurerm_storage_account.this.primary_web_endpoint }
+  blob_properties {
+    versioning_enabled = false
+  }
+
+  tags = var.tags
+}
