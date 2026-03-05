6758b7bde0ebbcbfe1de8f596d6dd6df5b8107b3
dash-bhavinmakwana <Bhavin.M@dashtechinc.com>
2026-03-05 17:30:01 +0530

    Create variables.tf

---
+++ modules/web_app/variables.tf
@@ -0,0 +1,25 @@
+variable "app_name" {
+  type        = string
+  description = "Name of the web application"
+}
+
+variable "env" {
+  type        = string
+  description = "Environment name (dev, prod, etc.)"
+}
+
+variable "rg_name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to all resources"
+  default     = {}
+}
