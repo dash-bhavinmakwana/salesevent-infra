869306d52da02031b5b3b4cc0f35f761be99d33e
dash-bhavinmakwana <Bhavin.M@dashtechinc.com>
2026-03-05 17:03:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,38 @@
-# This tells Terraform which version of the engine and providers to use globally
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0" # Prevents accidental upgrades to major versions
-    }
-  }
+# 1. Resource Group for the environment
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.project_name}-${var.env}"
+  location = var.location
 }
 
-# The default provider configuration
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+# 2. Call the Networking Module
+module "networking" {
+  source   = "./modules/networking"
+  rg_name  = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  env      = var.env
+}
+
+# 3. Call the Storage Module
+module "storage" {
+  source   = "./modules/storage"
+  st_name  = "st${var.project_name}${var.env}"
+  rg_name  = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+}
+
+# 4. Create the 2 Flutter Static Apps (Free)
+module "flutter_apps" {
+  for_each = toset(var.flutter_app_names)
+  source   = "./modules/static_app"
+  app_name = each.key
+  rg_name  = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+}
+
+# 5. Create the 1 Web App (F1 Free)
+module "main_web_app" {
+  source   = "./modules/web_app"
+  app_name = "${var.project_name}-web"
+  rg_name  = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
 }
