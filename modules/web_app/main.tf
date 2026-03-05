2cecb2fc871f11e7e2f94d34250f5d3ee9d5b6f6
dash-bhavinmakwana <Bhavin.M@dashtechinc.com>
2026-03-05 17:29:23 +0530

    Update main.tf

--- modules/web_app/main.tf
+++ modules/web_app/main.tf
@@ -1,24 +1,22 @@
-variable "app_name" {}
-variable "rg_name"  {}
-variable "location" {}
-
-resource "azurerm_service_plan" "free_plan" {
-  name                = "asp-${var.app_name}-free"
+resource "azurerm_service_plan" "this" {
+  name                = "asp-${var.app_name}-${var.env}"
   resource_group_name = var.rg_name
   location            = var.location
   os_type             = "Linux"
   sku_name            = "F1" # Free Tier
+
+  tags = var.tags
 }
 
-resource "azurerm_linux_web_app" "app" {
-  name                = "app-${var.app_name}"
+resource "azurerm_linux_web_app" "this" {
+  name                = "app-${var.app_name}-${var.env}"
   resource_group_name = var.rg_name
   location            = var.location
-  service_plan_id     = azurerm_service_plan.free_plan.id
+  service_plan_id     = azurerm_service_plan.this.id
 
   site_config {
-    always_on = false # Required for Free tier
+    always_on = false # Must be false on Free tier
   }
-}
 
-output "webapp_url" { value = azurerm_linux_web_app.app.default_hostname }
+  tags = var.tags
+}
